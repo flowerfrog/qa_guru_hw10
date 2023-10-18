fbebaa0894bc277ea4c565962294ce617d3fe5c2
flowerfrog <alexosenn@gmail.com>
2023-10-18 14:08:34 +0300

    Updating test_practice_form.py

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,43 +1,35 @@
-from selene import browser, have, command
-import os
-
-from selene.support.conditions import be
+from data.users import User
+from pages.registration_page import RegistrationPage
 
 
 def test_complete_practice_form():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type('test')
-    browser.element('#lastName').type('user')
-    browser.element('#userEmail').type('test@user.com')
-    browser.element('label[for="gender-radio-2"]').click()
-    browser.element('#userNumber').type('79999999999')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element('option[value="11"]').click()
-    browser.element('.react-datepicker__year-select').type('2000')
-    browser.element('.react-datepicker__day.react-datepicker__day--025').click()
-    browser.element('#subjectsInput').type('math').press_enter()
-    browser.element('label[for="hobbies-checkbox-2"]').click()
-    browser.element('//input[@type="file"]').type(os.path.abspath("resources/image.jpg"))
-    browser.element('#currentAddress').type('Ростов-сити, Забугорная 3к1')
-    browser.element('//*[@id="react-select-3-input"]').type('NCR').press_enter()
-    browser.element('//*[@id="react-select-4-input"]').type('Delhi').press_enter()
-    browser.element('//footer').perform(command.js.remove)
-    browser.all('#userForm > div').should(have.size(11))
-    browser.element('#submit').click()
-    browser.all('#todo-list>li').by(have.css_class('was-validated'))
-    browser.all(".table-dark>tbody>tr>td:nth-child(2)").should(have.texts(
-        "test user",
-        "test@user.com",
-        "Female",
-        "7999999999",
-        "25 December,2000",
-        "Maths",
-        "Reading",
-        "image.jpg",
-        "Ростов-сити, Забугорная 3к1",
-        "NCR Delhi"
-    ))
-    browser.element("#closeLargeModal").should(be.visible).click()
+    registration_page = RegistrationPage()
+
+    # GIVEN
+    user = User(
+        first_name='test',
+        last_name='user',
+        email='test@user.com',
+        gender='Female',
+        phone_number='7999999999',
+        month_of_birth='December',
+        year_of_birth='2000',
+        day_of_birth='25',
+        subject='Maths',
+        hobby='Reading',
+        picture='image.jpg',
+        current_address='Ростов-сити, Забугорная 3к1',
+        state='NCR',
+        city='Delhi'
+    )
+
+    registration_page.open()
+
+    # WHEN
+    registration_page.register(user)
+
+    # THEN
+    registration_page.user_should_registered(user)
 
 
 
