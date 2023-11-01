c4eb3d280270642597cc28992be781b817da1382
flowerfrog <alexosenn@gmail.com>
2023-11-01 10:30:39 +0300

    Added removing footer

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,12 +1,12 @@
-import os
 import resource
-
 from selene import browser, be, have, command
 
 
 class RegistrationPage:
     def open(self):
         browser.open('/automation-practice-form')
+        browser.execute_script('document.querySelector("#fixedban").remove()')
+        browser.element('footer').execute_script('element.remove()')
         return self
 
     def register(self, user):
@@ -27,7 +27,6 @@ class RegistrationPage:
         browser.element('#currentAddress').should(be.visible).type(user.current_address)
         browser.element("#react-select-3-input").should(be.visible).type(user.state).press_enter()
         browser.element("#react-select-4-input").should(be.visible).type(user.city).press_enter()
-        browser.element('//footer').perform(command.js.remove)
         browser.element("#submit").should(be.visible).click()
         return self
 
